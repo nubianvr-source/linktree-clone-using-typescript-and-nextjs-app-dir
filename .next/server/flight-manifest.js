5b2f78e8cfb0ccd4946f64fd74de3eff11a1adf6
Andy-Nubian <services.nubianvr@gmail.com>
2023-11-07 12:34:45 +0000

    Fixed name in description

--- .next/server/flight-manifest.js
+++ .next/server/flight-manifest.js
@@ -1,1 +1,279 @@
-self.__RSC_MANIFEST={"__ssr_module_mapping__":{"30":{"":{"id":"9797","name":"","chunks":["931:app/page-847d30526395367d"],"async":false},"*":{"id":"9797","name":"*","chunks":["931:app/page-847d30526395367d"],"async":false},"default":{"id":"9797","name":"default","chunks":["931:app/page-847d30526395367d"],"async":false}},"2092":{"":{"id":"6862","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"6862","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"6862","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"7043":{"":{"id":"2030","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"2030","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"2030","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"7954":{"":{"id":"9446","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"9446","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"9446","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"9875":{"":{"id":"3258","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"3258","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"3258","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}}},"__edge_ssr_module_mapping__":{},"__entry_css_files__":{"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\app\\layout":["static/css/780092ca01022f03.css"]},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\app-router.js":{"":{"id":"7954","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"7954","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"7954","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\error-boundary.js":{"":{"id":"7043","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"7043","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"7043","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\layout-router.js":{"":{"id":"9875","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"9875","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"9875","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\render-from-template-context.js":{"":{"id":"2092","name":"","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"*":{"id":"2092","name":"*","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false},"default":{"id":"2092","name":"default","chunks":["272:webpack-9f661d940827adcf","17:17-12efd9d6cbd244d3","744:main-app-ce231d9dd24fdb32"],"async":false}},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\image.js":{"":{"id":"30","name":"","chunks":["931:app/page-847d30526395367d"],"async":false},"*":{"id":"30","name":"*","chunks":["931:app/page-847d30526395367d"],"async":false},"default":{"id":"30","name":"default","chunks":["931:app/page-847d30526395367d"],"async":false}},"C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\styles\\globals.css":{"default":{"id":"6729","name":"default","chunks":["static/css/780092ca01022f03.css"]}}}
+self.__RSC_MANIFEST={
+  "__ssr_module_mapping__": {
+    "(app-client)/./node_modules/next/dist/client/image.js": {
+      "": {
+        "id": "(sc_client)/./node_modules/next/dist/client/image.js",
+        "name": "",
+        "chunks": [
+          "app/page:app/page"
+        ],
+        "async": false
+      },
+      "*": {
+        "id": "(sc_client)/./node_modules/next/dist/client/image.js",
+        "name": "*",
+        "chunks": [
+          "app/page:app/page"
+        ],
+        "async": false
+      },
+      "default": {
+        "id": "(sc_client)/./node_modules/next/dist/client/image.js",
+        "name": "default",
+        "chunks": [
+          "app/page:app/page"
+        ],
+        "async": false
+      }
+    },
+    "(app-client)/./node_modules/next/dist/client/components/app-router.js": {
+      "": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/app-router.js",
+        "name": "",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "*": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/app-router.js",
+        "name": "*",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "default": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/app-router.js",
+        "name": "default",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      }
+    },
+    "(app-client)/./node_modules/next/dist/client/components/error-boundary.js": {
+      "": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/error-boundary.js",
+        "name": "",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "*": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/error-boundary.js",
+        "name": "*",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "default": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/error-boundary.js",
+        "name": "default",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      }
+    },
+    "(app-client)/./node_modules/next/dist/client/components/layout-router.js": {
+      "": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/layout-router.js",
+        "name": "",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "*": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/layout-router.js",
+        "name": "*",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "default": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/layout-router.js",
+        "name": "default",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      }
+    },
+    "(app-client)/./node_modules/next/dist/client/components/render-from-template-context.js": {
+      "": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+        "name": "",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "*": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+        "name": "*",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      },
+      "default": {
+        "id": "(sc_client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+        "name": "default",
+        "chunks": [
+          "app-client-internals:app-client-internals"
+        ],
+        "async": false
+      }
+    }
+  },
+  "__edge_ssr_module_mapping__": {},
+  "__entry_css_files__": {
+    "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\app\\layout": [
+      "static/css/_app-client_styles_globals_css.css"
+    ]
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\image.js": {
+    "": {
+      "id": "(app-client)/./node_modules/next/dist/client/image.js",
+      "name": "",
+      "chunks": [
+        "app/page:app/page"
+      ],
+      "async": false
+    },
+    "*": {
+      "id": "(app-client)/./node_modules/next/dist/client/image.js",
+      "name": "*",
+      "chunks": [
+        "app/page:app/page"
+      ],
+      "async": false
+    },
+    "default": {
+      "id": "(app-client)/./node_modules/next/dist/client/image.js",
+      "name": "default",
+      "chunks": [
+        "app/page:app/page"
+      ],
+      "async": false
+    }
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\app-router.js": {
+    "": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/app-router.js",
+      "name": "",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "*": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/app-router.js",
+      "name": "*",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "default": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/app-router.js",
+      "name": "default",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    }
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\error-boundary.js": {
+    "": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/error-boundary.js",
+      "name": "",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "*": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/error-boundary.js",
+      "name": "*",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "default": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/error-boundary.js",
+      "name": "default",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    }
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\layout-router.js": {
+    "": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/layout-router.js",
+      "name": "",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "*": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/layout-router.js",
+      "name": "*",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "default": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/layout-router.js",
+      "name": "default",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    }
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\node_modules\\next\\dist\\client\\components\\render-from-template-context.js": {
+    "": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+      "name": "",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "*": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+      "name": "*",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    },
+    "default": {
+      "id": "(app-client)/./node_modules/next/dist/client/components/render-from-template-context.js",
+      "name": "default",
+      "chunks": [
+        "app-client-internals:app-client-internals"
+      ],
+      "async": false
+    }
+  },
+  "C:\\Users\\anspe\\OneDrive\\Documents\\GitHub\\linktree-clone-using-typescript-and-nextjs-app-dir\\styles\\globals.css": {
+    "default": {
+      "id": "null",
+      "name": "default",
+      "chunks": [
+        "static/css/_app-client_styles_globals_css.css"
+      ]
+    }
+  }
+}
--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,9 @@ export default function Head() {
   return (
     <>
     <title>Funeral LinkTree</title>
-    <meta name="description" content="ING. Godwin Korsie Sherrie Funeral LinkTree" />
+    <meta name="description" content="ING. Godwin Korshie Sherrie's Funeral LinkTree" />
     <link rel="icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
   </>
   );
